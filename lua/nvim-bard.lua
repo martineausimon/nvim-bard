7e97b99cf04dc71ceb63a9937e3620bce577b674
martineausimon <martineau.simon@gmail.com>
2023-07-17 23:46:23 +0200

    feat: Add python3 availability check

--- lua/nvim-bard.lua
+++ lua/nvim-bard.lua
@@ -8,7 +8,11 @@ end
 
 vim.api.nvim_create_user_command("Bard",
   function()
-    require('nvim-bard.module').toggle_bard()
+    if not vim.fn.has('python3') then
+      print('[nvim-bard] python3 is not available, please check ":help provider-python"')
+    else
+      require('nvim-bard.module').toggle_bard()
+    end
   end,
 {})
 
